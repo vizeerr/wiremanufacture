2cbce44609c01aec48de560de78ba748b15cdfbb
vivek sagar <vivek54540@gmail.com>
2024-09-26 23:58:38 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.logocrawler').slick({
+        slidesToShow: 6,      
+        slidesToScroll: 1,    
+        autoplay: true,        
+        autoplaySpeed: 1200,   
+        dots: false,            
+        arrows:false
+    })
+})
