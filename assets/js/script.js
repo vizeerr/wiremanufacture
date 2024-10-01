aa26b0c9d6be5081abe260d545a32c81c61bd2aa
vivek sagar <vivek54540@gmail.com>
2024-10-01 23:48:02 +0530

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,32 @@ $(document).ready(function(){
         autoplay: true,        
         autoplaySpeed: 1200,   
         dots: false,            
-        arrows:false
-    })
-})
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200, // for large devices
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 992, // for medium devices
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 768, // for small devices
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 576, // for extra small devices
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+        ]
+    });
+});
